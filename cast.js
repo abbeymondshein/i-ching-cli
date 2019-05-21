adfca2115491264943e50c27a0af0b2759e188da
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2019-05-20 22:23:39 -0400

    print trigram

--- cast.js
+++ cast.js
@@ -1,14 +1,40 @@
 // generate coin tosses that combine together to form hexagrams
 
-class Coin {
- toss() {
-   return (Math.floor(Math.random() * 2))
- }
- generateLine(){
+const toss = () => {
+  return Math.floor(Math.random() * 2) === 0 ? 3 : 2;
+};
+const generateLine = () => {
+  return toss() + toss() + toss();
+};
+const generateTrigram = () => {
+  return [generateLine(), generateLine(), generateLine()];
+};
+const printTrigram = () => {
+  const trigram = generateTrigram();
+  const toPrint = trigram.slice().reverse();
+  console.log(trigram, toPrint);
+  for (let i = 0; i < toPrint.length; i++) {
+    let current = toPrint[i];
+    switch (current) {
+      case 6:
+        console.log("--x--");
+        break;
+      case 7:
+        console.log("-----");
+        break;
+      case 8:
+        console.log("-- --");
+        break;
+      case 9:
+        console.log("--0--");
+        break;
+      default:
+        console.log(`didn't understand ${current}`);
+    }
+  }
+};
 
- }
-}
-
-const ex = new Coin;
-
-console.log(ex.toss());
+// console.log(toss());
+// console.log(generateLine());
+// console.log(generateTrigram());
+console.log(printTrigram());
