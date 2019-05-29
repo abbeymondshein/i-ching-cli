36ef012615c9e2a83e28c07e2c5ed03a7488d3df
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2019-05-29 15:31:14 -0400

    cli option config

--- cast.js
+++ cast.js
@@ -1,5 +1,17 @@
 // generate coin tosses that combine together to form hexagrams
 const hexagrams = require("./hexagrams");
+const program = require('commander');
+const c = require('chalk');
+const g = require('gradient-string');
+
+// * Configures options to pass for CLI
+program
+  .option('-c, --cast', 'Cast reading')
+  .parse(process.argv)
+
+  console.log(`You've chosen to:`)
+  if (program.cast) console.log(`Cast reading`)
+
 
 const toss = () => {
   return Math.floor(Math.random() * 2) === 0 ? 3 : 2;
@@ -12,10 +24,10 @@ const convertHexagrams = hexagram => {
   let primary = "";
   let relating = "";
   for (let i = 0; i < hexagram.length; i++) {
-    if (hexagram[i] === '6') {
+    if (hexagram[i] === "6") {
       primary += 8;
       relating += 7;
-    } else if (hexagram[i] === '9') {
+    } else if (hexagram[i] === "9") {
       primary += 7;
       relating += 8;
     } else {
@@ -25,8 +37,8 @@ const convertHexagrams = hexagram => {
   }
   // console.log("primary:  ", primary, locateHexagram(primary));
   // console.log("relating:  ", relating, locateHexagram(relating));
-  logFormat(locateHexagram(primary))
-  logFormat(locateHexagram(relating))
+  logFormat(locateHexagram(primary),"Primary");
+  logFormat(locateHexagram(relating), "Relating");
 };
 
 const generateHexagram = () => {
@@ -40,20 +52,21 @@ const generateHexagram = () => {
 const locateHexagram = hex => {
   hex = Number(hex);
   const found = hexagrams.find(e => {
-    return e.pattern === hex
+    return e.pattern === hex;
   });
   return found;
 };
 
-const logFormat = result => {
-  console.log(`  ${result.symbol}
-  ${result.name.en}, ${result.name.zh}
-  ${result.image}
-  Judgement:
-  ${result.judgment}
-   `)
-
-}
+const logFormat = (result, label) => {
+  console.log(`
+  ${c.cyan(label, ':')}
+  ${c.bgCyan.bold('   ',result.symbol,'   ')}
+  ${c.bgCyan.underline(result.name.en, result.name.zh)}
+  ${g.vice.multiline(result.image)}
+  ${c.bgMagenta('Judgement:')}
+  ${g.vice.multiline(result.judgment)}
+   `);
+};
 // console.log(toss());
 // console.log(generateLine());
 console.log(generateHexagram());
